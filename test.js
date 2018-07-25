5d0e4a81cf151ee8fc97d8800c3d7bc91020ba53
lauren-hart <laurenannehart@gmail.com>
2018-07-26 10:35:29 +1200

    create all the buttons for the calculator in html

---
+++ test.js
@@ -0,0 +1,30 @@
+
+//EQUALS FUNCTION ---------------------------------------
+
+function equals () {
+
+}
+
+//ADD FUNCTION ---------------------------------------
+
+function add () {
+
+}
+
+//SUBTRACT FUNCTION ---------------------------------------
+
+function subtract () {
+
+}
+
+//MULTIPLY FUNCTION ---------------------------------------
+
+function multiply () {
+
+}
+
+//DIVIDE FUNCTION ---------------------------------------
+
+function divide () {
+
+}
