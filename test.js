1b6e42df30e6e1de1acab08d83dd9ba4de27f033
lauren-hart <laurenannehart@gmail.com>
2018-07-28 16:30:47 +1200

    update html to include form/input tags. Display now shows.

--- test.js
+++ test.js
@@ -1,47 +1,29 @@
-
-
 var entries = [];
 var total = 0;
 
-var display = '';
+var string = '';
 
-function pressButton () {
+function display (num) {
 
-
-
- var getNumber = document.getElementsByClassName("display");
- var displayScreen = document.getElementsByClassName("number");
-  
-
- for (i = 0; i < getNumber.length; i++) {
-    getNumber[i].innerHTML = displayScreen[i].innerHTML;
- }
+    document.getElementById('displayScreen').value 
+    = document.getElementById('displayScreen').value + num
 
 }
 
 
-/*
-
-var entries = [];
-var total = 0;
-
-var display = '';
-
-function pressButton () {
 
 
 
- var x = document.getElementsByClassName("display");
- var y = document.getElementsByClassName("number");
+
+
+
+
+
+
+
+  
   
 
- for (i = 0; i < x.length; i++) {
-     x[i].innerHTML = "?"
-
- }
 
 
-console.log(y);
 
-}
-*/
