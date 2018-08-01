3e831099d9d32fcf5ddcc2d0f1b1838a2839465f
lauren-hart <laurenannehart@gmail.com>
2018-08-02 08:48:43 +1200

    completed clear function and convert val to number

--- test.js
+++ test.js
@@ -1,64 +1,85 @@
-//CALCULATOR, YOU WILL NEED AN EMPTY ARRAY TO HOLD ENTRIES
-//YOU WILL NEED AN EMPTY STRING WHICH WILL ACT AS THE LAST
-//BUTTON TO BE PRESSED
-//YOU WILL NEED TO SET THE TOTAL TO 0
 
-var entries = [];
-var total = 0;
-var temp = '';
+//When the DOM content has been loaded the start
+//function will run
 
-//YOU NEED TO ADD AN EVENT LISTENER
-//FOR WHEN A BUTTON IS CLICKED
-//YOU DON'T HAVE ANY CLASSES OR ID
-//ON YOUR BUTTONS SO TRY USING
-//GET ELEMENT BY TAG
+document.addEventListener('DOMContentLoaded', start)
 
+//The start function will run a function called
+//addEventListener
 
+function start() {
+    addEventListener()
+ }
+ 
+//The function addEventListener will then run
+//to add a click event to the buttons
 
+ function addEventListener () {
 
+    var val = document.getElementsByTagName("button");
 
-function myFunction (butPressed) {
-
-    var isNumber = parseInt(butPressed);
-
-
-    if (isNaN(isNumber) === false) {
-        document.getElementById("displayScreen").innerHTML = 
-        document.getElementById("displayScreen").innerHTML + butPressed;
-    
-    } else if (butPressed === '+') {
-        console.log("this is an addition");
-    
-    } else if (butPressed === '-') {
-        console.log("this is a subtraction");
-    
-    } else if (butPressed === '*') {
-        console.log("this is a multiplication");
-    
-    } else if (butPressed === '/') {
-        console.log("this is a division");
-    
-    } else if (butPressed === '.') {
-        console.log("this is a decimal");
-    
-    } else if (butPressed === 'AC') {
-        console.log("this to clear all");
-    
-    } else if (butPressed === 'CE') {
-        console.log("this is to clear the last entry");
+        for (var i = 0; i < val.length; i++) {
+            val[i].addEventListener("click", calculator)
+        }
     }
 
+ //Need an empty string variable 
+
+ var temp = '';
+ var total = 0;
+
+
+
+ //Calculator function
+
+ function calculator (evt) {
+
+
+//This will pull the innerHTML from the button
+//that has been clicked
+
+    val = evt.target.innerText;
+
+//This will convert a digit to a number
+
+    var len = document.getElementsByClassName("digit");
+
+    for (var i = 0; i < len.length; i++) {
+        if (len[i].innerHTML === val){ //To match it to val pressed
+            var val = parseInt(len[i].innerHTML);
+        }
+    } 
+
+//This will take the what is currently displayed on
+//display and append each subsequent button pressed
+
+    console.log(val);
+
+    document.getElementById("displayScreen").innerHTML
+    = document.getElementById("displayScreen").innerHTML + val;
+
+//First we check if the value is a number or a decimal
+//If it is we append them to the string temp
+
+    if (!isNaN(val) || val === '.') {
+        var temp = document.getElementById("displayScreen").innerHTML;
+    } else if (val === 'AC') {
+        var temp = '';
+        var total = 0;
+        document.getElementById("displayScreen").innerHTML = 0;
+    } else if (val === '+') {
+        console.log("this is a plus");
+    }
+
+    console.log(temp);
+
+//You need to add an id or class or something so that
+//you can distinguish digits and then you can turn 
+//them into an integer
+
+ 
+//Function to reset values back to zero
+
+
+
 }
-
-
-/*
-document.getElementById("displayScreen").innerHTML = 
-document.getElementById("displayScreen").innerHTML + butPressed; */
-
-
-
-
-
-
-
-
