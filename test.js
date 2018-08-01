e178aac2a3008b8c1325ca64bea7b1ce44a8ac65
lauren-hart <laurenannehart@gmail.com>
2018-08-01 14:29:42 +1200

    calculator displays key pressed using innerText

--- test.js
+++ test.js
@@ -1,32 +1,26 @@
-
+//CALCULATOR, YOU WILL NEED AN EMPTY ARRAY TO HOLD ENTRIES
+//YOU WILL NEED AN EMPTY STRING WHICH WILL ACT AS THE LAST
+//BUTTON TO BE PRESSED
+//YOU WILL NEED TO SET THE TOTAL TO 0
 
 var entries = [];
 var total = 0;
+var temp = '';
 
-var string = '';
+//YOU NEED TO ADD AN EVENT LISTENER
+//FOR WHEN A BUTTON IS CLICKED
+//YOU DON'T HAVE ANY CLASSES OR ID
+//ON YOUR BUTTONS SO TRY USING
+//GET ELEMENT BY TAG
 
-function display (num) {
 
-    document.getElementById('displayScreen').value 
-    = document.getElementById('displayScreen').value + num
+
+function myFunction (butPressed) {
     
+    document.getElementById("displayScreen").innerHTML = butPressed;
+
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
-  
-
 
 
 
